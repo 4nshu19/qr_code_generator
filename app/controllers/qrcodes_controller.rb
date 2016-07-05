e708bd45a5cfe7f08b9e5690096c7d7a21a1f06c
Anshu Rathee <4nshu19@gmail.com>
2016-07-05 10:35:26 +0530

    added login/out

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -1,6 +1,6 @@
 class QrcodesController < ApplicationController
   before_action :set_qrcode, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /qrcodes
   # GET /qrcodes.json
   def index
